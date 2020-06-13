b555c2cbcd05358a70e1da421e9ef357e583897e
EmendiG <dus.mateusz.s@gmail.com>
2020-06-13 23:50:21 +0200

    RealITy 0.2020.02.01

--- osmOverpassApi.py
+++ osmOverpassApi.py
@@ -1,5 +1,6 @@
 import overpass
 import folium
+import numpy as np
 from geojson import Point, Feature, FeatureCollection, Polygon, MultiPolygon, dump
 
 
@@ -86,53 +87,95 @@ class District:
     def mapDistrictPolygons(self):
         responseCity = self.get_districts_osmApi()
         dlugosc = len(list({dis['id']: dis for dis in responseCity['elements']}.values()))
-        features = []
+
+        # set up base coordinates for folium.Map.__init__ befor appending gjson
+        coordynatLat = []
+        coordynatLon = []
+        for dzielnice in range(0, dlugosc):
+            for cLatLon in responseCity['elements'][dzielnice]['members']:
+                if 'geometry' in cLatLon:
+                    coordynatLat.append(cLatLon['geometry'][0]['lat'])
+                    coordynatLon.append(cLatLon['geometry'][0]['lon'])
+        coordynat = (np.mean(coordynatLat), np.mean(coordynatLon))
+
+        map = folium.Map(
+            location=coordynat,
+            zoom_start=11
+        )
+
         for dzielnice in range(0, dlugosc):
             odpowiedz = []
-            coords = []
             for num, ref in enumerate(responseCity['elements'][dzielnice]['members']):
                 if ref['role'] == 'outer' and ref['type'] == 'way':
                     odpowiedz.append(ref)
+            coords = self.getCoordinatesFromOSM_Districts(odpowiedz)
 
-            matchingCounter = len(odpowiedz) - 1
-
-            for nmb, geometry in enumerate(odpowiedz):
-                if nmb == 0:
-                    coords += [(float(geom['lon']), float(geom['lat'])) for geom in geometry['geometry']]
-            while matchingCounter > 0:
-                for nmb, geometry in enumerate(odpowiedz):
-                    if nmb > 0:
-                        koordynat_0 = coords[-1][0]
-                        koordynat_1 = coords[-1][1]
-                        if koordynat_0 == odpowiedz[nmb]['geometry'][0]['lon'] and koordynat_1 == \
-                                odpowiedz[nmb]['geometry'][0]['lat']:
-                            coords += [(float(geom['lon']), float(geom['lat'])) for geom in geometry['geometry']]
-                            matchingCounter -= 1
-                        elif koordynat_0 == odpowiedz[nmb]['geometry'][len(odpowiedz[nmb]['geometry']) - 1][
-                            'lon'] and koordynat_1 == \
-                                odpowiedz[nmb]['geometry'][len(odpowiedz[nmb]['geometry']) - 1]['lat']:
-                            coords += [(float(geom['lon']), float(geom['lat'])) for geom in geometry['geometry'][::-1]]
-                            matchingCounter -= 1
-
-            multi1 = MultiPolygon(coords)
-            poly1 = Polygon(multi1)
-            multi2 = MultiPolygon(poly1)
-            features.append(Feature(geometry=multi2))
-
-        with open('test_cord.geojson', 'w', encoding='utf-8') as file:
-            feature_collection = FeatureCollection(features)
-            dump(feature_collection, file)
-
-        coordynat = [coords[0][1], coords[0][0]]
-        map = folium.Map(
-            location=coordynat,
-            zoom_start=10
-        )
-
-        folium.GeoJson('test_cord.geojson').add_to(map)
-        folium.LayerControl().add_to(map)
+            district = 'lol'
+            geojson = {
+                "type": "FeatureCollection",
+                "features": [
+                    {
+                        "type": "Feature",
+                        "geometry": {
+                            "type": "Polygon",
+                            "name": district,
+                            "coordinates": [[[d[0], d[1]] for d in coords]],
+                        },
+                        "properties": {'name': district},
+                    }]
+            }
+            folium.GeoJson(geojson).add_to(map)
         map.save(f"districts/{self.miasto}.html")
-        return map
+
+    def getCoordinatesFromOSM_Districts(self, odpowiedz):
+
+        #features = []
+        #for dzielnice in range(0, dlugosc):
+        #odpowiedz = []
+        #coords = []
+        #for num, ref in enumerate(responseCity['elements'][dzielnice]['members']):
+        #    if ref['role'] == 'outer' and ref['type'] == 'way':
+        #        odpowiedz.append(ref)
+        matchingCounter = len(odpowiedz) - 1
+        coords = []
+        for nmb, geometry in enumerate(odpowiedz):
+            if nmb == 0:
+                coords += [(float(geom['lon']), float(geom['lat'])) for geom in geometry['geometry']]
+        while matchingCounter > 0:
+            for nmb, geometry in enumerate(odpowiedz):
+                if nmb > 0:
+                    koordynat_0 = coords[-1][0]
+                    koordynat_1 = coords[-1][1]
+                    if koordynat_0 == odpowiedz[nmb]['geometry'][0]['lon'] and koordynat_1 == \
+                            odpowiedz[nmb]['geometry'][0]['lat']:
+                        coords += [(float(geom['lon']), float(geom['lat'])) for geom in geometry['geometry']]
+                        matchingCounter -= 1
+                    elif koordynat_0 == odpowiedz[nmb]['geometry'][len(odpowiedz[nmb]['geometry']) - 1][
+                        'lon'] and koordynat_1 == \
+                            odpowiedz[nmb]['geometry'][len(odpowiedz[nmb]['geometry']) - 1]['lat']:
+                        coords += [(float(geom['lon']), float(geom['lat'])) for geom in geometry['geometry'][::-1]]
+                        matchingCounter -= 1
+
+            #multi1 = MultiPolygon(coords)
+            #poly1 = Polygon(multi1)
+            #multi2 = MultiPolygon(poly1)
+            #features.append(Feature(geometry=multi2))
+
+        #with open('test_cord.geojson', 'w', encoding='utf-8') as file:
+        #    feature_collection = FeatureCollection(features)
+        #    dump(feature_collection, file)
+
+        #coordynat = [coords[0][1], coords[0][0]]
+        #map = folium.Map(
+        #    location=coordynat,
+        #    zoom_start=10
+        #)
+
+        #folium.GeoJson('test_cord.geojson').add_to(map)
+        #folium.LayerControl().add_to(map)
+        #map.save(f"districts/{self.miasto}.html")
+        #return map
+        return coords
 
 
 
