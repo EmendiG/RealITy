0b03ca8704727ec728e9df7d7195eb4744a7dae7
EmendiG <dus.mateusz.s@gmail.com>
2020-09-13 20:33:33 +0200

    RealITy 0.2020.03.09 (developing frontend css + html)

--- RealityWeb/RealityWeb/static/thema/js/chosecity.js
+++ RealityWeb/RealityWeb/static/thema/js/chosecity.js
@@ -1,1 +1,9 @@
 console.log('hello world')
+
+$(document).on("click", "a", function() {
+    if($(this).attr("targetLink")=="yes"){
+        document.cookie = "city="+$(this).attr("targetCity");
+        console.log(document.cookie)
+    }
+});
+
--- RealityWeb/RealityWeb/views.py
+++ RealityWeb/RealityWeb/views.py
@@ -22,8 +22,6 @@ def index(request):
     }
     return render(request, 'home/index.html', context)
 
-def home(request):
-    return render(request, 'miasta/Warszawa.html')
 
 def about(request):
     context ={
@@ -33,18 +31,45 @@ def about(request):
 
 
 def show_map(request):
-    form = GetPriceForm(request.POST)
+
+    latlonDict = {
+                    'warszawa':[52.237, 21.017], 
+                    'krakow':[50.05, 19.945],
+                    'lodz': [51.76, 19.457],  
+                    'wroclaw': [51.108, 17.039], 
+                    'poznan': [52.409, 16.932], 
+                    'gdansk': [54.372, 18.638], 
+                    'szczecin': [53.428, 14.553], 
+                    'bydgoszcz': [53.123, 18.008], 
+                    'lublin': [51.246, 22.568],
+                    'bialystok': [53.118, 23.126]
+    }
+    if request.COOKIES['city']:
+        latlon = latlonDict[request.COOKIES['city']]
+
     # print('sessionid: ', request.COOKIES)
     # print('\n_auth_user_hash: ', request.session.get('_auth_user_hash'))
 
-    if form.is_valid():
-        form.save()
-        price = test.RealityPython_MLREP_getPrice(request)
-        messages.success(request, f"Estymowana cena za metr kwadratowy wynosi {round(price,2)} PLN boghactwo :D ")
+    if request.method == 'POST':
+        form = GetPriceForm(request.POST)
+        if form.is_valid():
+            form.save()
+            price = test.RealityPython_MLREP_getPrice(request)
+            messages.success(request, f"Estymowana cena za metr kwadratowy wynosi {round(price,2)} PLN boghactwo :D ")
+            context = {
+            'title': 'Map',
+            'form': form,
+            'latlon': latlon,
+            'price': round(price,2)
+            }
+            return render(request, 'miasta/map.html', context)
+    else:
+        form = GetPriceForm()
 
     context = {
-               'title': 'Map',
-               'form': form
+            'title': 'Map',
+            'form': form,
+            'latlon': latlon
     }
     return render(request, 'miasta/map.html', context)
 
